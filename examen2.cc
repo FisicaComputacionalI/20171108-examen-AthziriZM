f8639550e4db53dd09c23e0558d4bcf9e472fa41
Athziri <31254627+AthziriZM@users.noreply.github.com>
2017-11-10 13:59:15 -0600

    Add files via upload

---
+++ examen2.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x=1;
+
+  cout<<"Dame un numero entero "<<endl;
+  cin>>x;
+
+  while(x<=5)
+  x%3==0;
+    if(x%3==0);
+    x=x+2;
+    if(x%3>0)
+      cout<<"print "<<x<<endl;
+    cin>>x;
+    x=x+2;
+  }
+  if(x>5)  
+return 0;
+}
+
+
+
+
+
---
+++ velocidad.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int d=0;
+  int t=0;
+  int divisor=0;
+  int v=0;
+
+  cout<<"Dame distancia "<<endl;
+  cin>>d;
+  cout<<"Dame tiempo "<<endl;
+  cin>>t;
+
+  if(d/t=v; v++)
+    cout<<"La velocidad es "<<endl;
+  retur 0;
+}

519f75a41f3d8530c150121b2891bce1a2ad461d
Isabel Pedraza <isapedraza@hotmail.com>
2018-01-08 20:30:50 +0100

    correccion de examen

--- examen2.cc
+++ examen2.cc
@@ -8,16 +8,16 @@ int main()
   cout<<"Dame un numero entero "<<endl;
   cin>>x;
 
-  while(x<=5)
-  x%3==0;
-    if(x%3==0);
-    x=x+2;
+  while(x<=5){
+    x%3==0;
+    // if(x%3==0)
+    //  x=x+2;
     if(x%3>0)
       cout<<"print "<<x<<endl;
-    cin>>x;
+    //cin>>x;
     x=x+2;
   }
-  if(x>5)  
+// if(x>5)  
 return 0;
 }
 
